181c13067f71412b38efd7bac63711bb51082586
Jiongxuan <875385920@qq.com>
2019-08-16 23:34:41 +0800

    Enable cors

---
+++ src/main/java/com/gaojx/weather/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.gaojx.weather.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowCredentials(true)
+				.allowedHeaders("*")
+				.allowedMethods("*")
+				.maxAge(3600);
+	}
+
+}
